a48c59f63377a9ea8ba8047d21a255cfab7fd006
willdeuschle <wjdeuschle@gmail.com>
2017-01-12 08:59:28 -0500

    added about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,6 +13,19 @@ export default class About extends Component {
                 </div>
                 <hr className='title-divider' />
                 <img src={willlaugh4food} alt='Will' className='will-about-image' />
+                <div className='will-title'>
+                    Will Deuschle
+                </div>
+                <div className='role-desc'>
+                    Full Stack Software Engineer
+                </div>
+                <hr className='role-divider' />
+                <div className='desc-para'>
+                    Software Engineer from Buffalo, New York, undergraduate at Harvard University. Currently on a year off working at a startup in Washington, D.C. called <a href='https://www.quorum.us' target='_blank' className='quorum-link' >Quorum</a>.
+                </div>
+                <div className='desc-para'>
+                    Passionate about front-end design, functional programming, and mobile development.
+                </div>
             </div>
         )
     }
--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -6,11 +6,15 @@ import About from './About'
 const shallowSetup = (props={}) => shallow(<About {...props} />)
 
 describe('<About />', () => {
-    it('should render itself, a snapshot, a small divider, an image', () => {
+    it('should render itself, a snapshot, a small divider, an image, a title, a role description, a role divider, description paragraphs', () => {
         const renderedAbout = shallowSetup()
         expect(renderedAbout.find('.About').length).toBe(1)
         expect(renderedAbout.find('.snapshot-title').length).toBe(1)
         expect(renderedAbout.find('.title-divider').length).toBe(1)
         expect(renderedAbout.find('.will-about-image').length).toBe(1)
+        expect(renderedAbout.find('.will-title').length).toBe(1)
+        expect(renderedAbout.find('.role-desc').length).toBe(1)
+        expect(renderedAbout.find('.role-divider').length).toBe(1)
+        expect(renderedAbout.find('.desc-para').length).toBeGreaterThanOrEqual(1)
     });
 });
