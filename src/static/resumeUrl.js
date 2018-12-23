abda667ac7d881e167b6356b7ea504fcc3b412ce
Will Deuschle <wjdeuschle@gmail.com>
2018-12-23 09:43:40 -0500

    update resume url

--- src/static/resumeUrl.js
+++ src/static/resumeUrl.js
@@ -1,2 +1,2 @@
-const resumeUrl = 'https://drive.google.com/open?id=0B2OF3MLVPR03czItbGNXbGg2NzQ'
+const resumeUrl = 'https://drive.google.com/file/d/12mmRgGAa40IVGf-iW_vR1o4jnak3R9r7/view'
 export default resumeUrl
